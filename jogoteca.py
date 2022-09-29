a17bdd21d6360dd4e7e1cc113a03a1a55ded1fe3
Nathalia <nathaliasantospbg@hotmail.com>
2022-09-29 14:46:02 -0300

    segundo commit

--- jogoteca.py
+++ jogoteca.py
@@ -1,8 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Combat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
 
-@app.route('/inicio')
-def ola():
-    return render_template('lista.html', titulo='Jogos')
-app.run()
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+app.run(debug=True)
